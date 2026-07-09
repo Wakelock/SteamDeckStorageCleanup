62feed614c30a8dbaeab8345615b6b92844e54cc
Wakelock <263392226+Wakelock@users.noreply.github.com>
2026-07-09 15:22:12 +0000

    Update SteamDeckStorageCleanup.sh

--- SteamDeckStorageCleanup.sh
+++ SteamDeckStorageCleanup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-# Steam Deck Storage Cleanup (2026 version)
-# Note: this script should only be run if one desperately needs storage.
-# If there's still too little space afterwards, try replacing flatpaks with native apps.
+# Steam Deck Storage Cleanup
 
-if [ "$(curl https://raw.githubusercontent.com/Wakelock/SteamDeckStorageCleanup/refs/heads/main/version.txt)" = "20260621" ]
+case "$1" in
+-update) if [ "$(curl https://raw.githubusercontent.com/Wakelock/SteamDeckStorageCleanup/refs/heads/main/version.txt)" = "20260709" ]
 then
-  echo "The script is up to date."
+  echo "The cleanup script is up to date."
 else
-  echo "Warning: unless you're offline, this version might be outdated."
+  echo "Warning: you're offline, or this version is outdated."
 fi
+exit
+esac
+
+prev_storage=$(df --output=avail -B1 /home | tail -1)
 
 # General cleanup
 flatpak uninstall --unused
@@ -21,23 +24,28 @@ rm -rf ~/.steam/steam/depotcache
 rm -rf ~/.steam/steam/steamapps/downloading
 rm -rf ~/.steam/steam/steamapps/shadercache
 
-# Sudo cleanup (optional)
+# Optional
+case "$1" in
+-sudo)
 sudo steamos-readonly disable
+echo Deleting cache...
 sudo chattr -i /var/cache
-sudo pacman -Scc
+sudo pacman -Scc <<<'y'
 sudo rm -rf ~/.cache
 sudo rm -rf /home/lost+found
 sudo rm -rf /var/cache
 sudo rm -rf /var/log
-
-# Swap cleanup (optional)
+echo Uninstalling bloatware...
+sudo pacman -R cups-pdf <<<'y'
+sudo pacman -R cups <<<'y'
+sudo pacman -R okular <<<'y'
+sudo pacman -R plasma-meta <<<'y'
+sudo pacman -R plasma-welcome <<<'y'
+echo Disbling Swap...
 sudo swapoff --all
 sudo rm -r /home/swapfile
+esac
 
-# System bloatware cleanup (optional)
-sudo pacman -R cups-pdf
-sudo pacman -R cups
-sudo pacman -R kdeconnect
-sudo pacman -R okular
-sudo pacman -R plasma-meta
-sudo pacman -R plasma-welcome
+curr_storage=$(df --output=avail -B1 /home | tail -1)
+freed_storage=$(( curr_storage - prev_storage ))
+echo "Successfully freed $(( freed_storage )) KB"
